c7bfcdcf602fb075431748f3959c4f0458efa008
Oyugi <119124852+oynoah@users.noreply.github.com>
2024-02-13 10:53:32 +0300

    Add files via upload

---
+++ query_2_number_of_days_visited_by_each_customer.sql
@@ -0,0 +1,6 @@
+/* How many days has each customer visited the restaurant */
+SELECT customer_id AS customer_id,
+	   COUNT(DISTINCT order_date) AS number_number_of_days
+FROM sales
+GROUP BY customer_id
+ORDER BY COUNT(DISTINCT order_date) DESC;
