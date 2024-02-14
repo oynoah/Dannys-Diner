d1c08c659b06be76dfdeb823e387d5f319764abd
Oyugi <119124852+oynoah@users.noreply.github.com>
2024-02-14 13:23:07 +0300

    Update query_1_total_amount_each_customer_spent_at_the_restaurant.sql

--- query_1_total_amount_each_customer_spent_at_the_restaurant.sql
+++ query_1_total_amount_each_customer_spent_at_the_restaurant.sql
@@ -1,19 +1,29 @@
 /* What is the total amount each customer spent at the restaurant */
-WITH total_quantity AS (SELECT product_id AS product_id,
-							   customer_id AS customer_id,
-							   COUNT(*) AS total_quantity
-						FROM sales
-						GROUP BY product_id,customer_id
-						ORDER BY total_quantity),
-	 menu_prices AS (SELECT product_id AS product_id,
-                            price AS price
-					 FROM menu),
-	 total_revenue AS (SELECT total_quantity.product_id AS product_id,
-							  menu_prices.price*total_quantity.total_quantity AS total_revenue
-                       FROM total_quantity JOIN menu_prices USING(product_id))
+WITH total_quantity AS (
+	SELECT 
+	    product_id AS product_id,
+	    customer_id AS customer_id,
+	    COUNT(*) AS total_quantity
+	FROM sales
+	GROUP BY product_id,customer_id
+	ORDER BY total_quantity
+),
+menu_prices AS (
+	SELECT 
+	    product_id AS product_id,
+            price AS price
+	FROM menu
+),
+total_revenue AS (
+	SELECT 
+	    total_quantity.product_id AS product_id,
+	    menu_prices.price*total_quantity.total_quantity AS total_revenue
+        FROM total_quantity JOIN menu_prices USING(product_id)
+)
 
-SELECT total_quantity.customer_id AS customer_id,
-	   SUM(total_revenue.total_revenue) AS total_revenue
+SELECT 
+    total_quantity.customer_id AS customer_id,
+    SUM(total_revenue.total_revenue) AS total_revenue
 FROM total_quantity JOIN total_revenue USING(product_id)
 GROUP BY total_quantity.customer_id
 ORDER BY SUM(total_revenue.total_revenue) DESC;
