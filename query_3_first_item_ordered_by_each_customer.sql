4c34428d6e79cb950b26ef328a4c2063aae909db
Oyugi <119124852+oynoah@users.noreply.github.com>
2024-02-14 13:24:17 +0300

    Update query_3_first_item_ordered_by_each_customer.sql

--- query_3_first_item_ordered_by_each_customer.sql
+++ query_3_first_item_ordered_by_each_customer.sql
@@ -1,6 +1,7 @@
 /* What was the first item from the menu purchased by each customer */
-SELECT DISTINCT sales.customer_id, 
-       menu.product_name
+SELECT 
+    DISTINCT sales.customer_id, 
+    menu.product_name
 FROM sales JOIN menu USING(product_id)
 WHERE sales.order_date = (SELECT MIN(sales.order_date)
                           FROM sales);
