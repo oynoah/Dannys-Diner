768ae6a22ad46c20b9d5531e6ca389ba3f882bdd
Oyugi <119124852+oynoah@users.noreply.github.com>
2024-02-14 13:24:58 +0300

    Update query_2_number_of_days_visited_by_each_customer.sql

--- query_2_number_of_days_visited_by_each_customer.sql
+++ query_2_number_of_days_visited_by_each_customer.sql
@@ -1,6 +1,7 @@
 /* How many days has each customer visited the restaurant */
-SELECT customer_id AS customer_id,
-	   COUNT(DISTINCT order_date) AS number_number_of_days
+SELECT 
+    customer_id AS customer_id,
+    COUNT(DISTINCT order_date) AS number_number_of_days
 FROM sales
 GROUP BY customer_id
 ORDER BY COUNT(DISTINCT order_date) DESC;
